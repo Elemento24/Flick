fd91840282baae01af2283f3841fb3d9292b207f
EC2 Default User <ec2-user@ip-172-31-13-36.us-east-2.compute.internal>
2020-10-26 17:01:49 +0000

    Modify the Show Page

--- src/components/Show/Show.js
+++ src/components/Show/Show.js
@@ -2,6 +2,10 @@ import React, {Fragment, useEffect} from 'react';
 import {connect} from 'react-redux';
 
 import * as actions from '../../store/actions/action';
+import { ReactComponent as Clock } from '../../assets/clock.svg';
+import { ReactComponent as Star } from '../../assets/show-star.svg';
+import { ReactComponent as Calender } from '../../assets/calendar.svg';
+import {convertDate} from '../../shared/utility';
 
 const ShowMovie = props => {
     const ID = props.match.params.id;
@@ -13,14 +17,38 @@ const ShowMovie = props => {
     }, [onGetMovie, ID]);
     
     if(movie){
+        const {date, month, year} = convertDate(movie.release_date);
+        
         movieDiv = (
             <Fragment>
                 <div className='show__title-box'>
-                    <h1 className='heading__pri show__title'>{movie.original_title}</h1>
+                    <h1 className='heading-pri show__title'>{movie.title}</h1>
+                    {movie.tagline ? <p className='show__tag'>-{movie.tagline}</p> : null}
                 </div>
                 <div className='show__movie'>
                     <div className='show__content'>
-                        <h1 className='heading__pri show__title'>{movie.original_title}</h1>
+                        <div className = "show__overview">{movie.overview}</div>
+                        <div className='show__genres'>
+                            {movie.genres.map(g => g.name).join(',  ')}
+                        </div>
+                        <div className = 'show__release-date'>
+                            <Calender className = "show__release-icon" />
+                            <div className='show__date'>{date}</div>
+                            <div className='show__my'>
+                                <span>{month}</span>
+                                <span>({year})</span>
+                            </div>
+                        </div>
+                        <div className='show__stats'>
+                            <div className='show__runtime'>
+                                <Clock className = "show__clock-icon" />
+                                <div className='show__time'><span>{movie.runtime}</span> Minutes</div>
+                            </div>  
+                            <div className='show__rating'>
+                                <Star className = "show__star-icon" />
+                                <div className='show__star'><span>{movie.vote_average}</span> ({movie.vote_count})</div>
+                            </div>
+                        </div>
                     </div>
                     <div className='show__img-box'>
                         <div className = 'show__img-div'>
@@ -36,7 +64,7 @@ const ShowMovie = props => {
     }
     
     return (
-        <main className="show">
+        <main className="show mb-10">
             {movieDiv}
         </main>
     );
--- src/shared/utility.js
+++ src/shared/utility.js
@@ -4,3 +4,14 @@ export const updateObject = (oldObject, updatedProperties) => {
         ...updatedProperties
     };
 };
+
+export const convertDate = (date) => {
+    const rel = date.split('-').map(el => parseInt(el, 10));
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const month = months[rel[1]-1];
+    return {
+        date: rel[2],
+        month,
+        year: rel[0]
+    };
+};
